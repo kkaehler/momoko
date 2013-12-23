dcb58a8ca992d9a382b991a60cddbec18156f7ea
Kip Kaehler <kip@nextdoor.com>
2013-12-23 14:09:17 -0800

    Create lazily-instantiated pool. Handle connection failures.

--- momoko/connection.py
+++ momoko/connection.py
@@ -9,6 +9,8 @@ Copyright 2011-2013 by Frank Smit.
 MIT, see LICENSE for more details.
 """
 
+import random
+
 from functools import partial
 from collections import deque
 
@@ -17,10 +19,10 @@ from psycopg2.extras import register_hstore as _psy_register_hstore
 from psycopg2.extensions import (connection as base_connection, cursor as base_cursor,
     POLL_OK, POLL_READ, POLL_WRITE, POLL_ERROR, TRANSACTION_STATUS_IDLE)
 
-from tornado import gen
-from tornado.ioloop import IOLoop
+from tornado import ioloop as tornado_ioloop
 
-from .exceptions import PoolError
+from .utils import log
+from . import exceptions
 
 
 # The dummy callback is used to keep the asynchronous cursor alive in case no
@@ -31,6 +33,7 @@ def _dummy_callback(cursor, error):
 
 
 class Pool:
+
     """
     Asynchronous connection pool.
 
@@ -45,47 +48,142 @@ class Pool:
         A callable that's called after all the connections are created. Defaults to ``None``.
     :param ioloop: An instance of Tornado's IOLoop. Defaults to ``None``.
     """
+
     def __init__(self,
-        dsn,
-        connection_factory=None,
-        size=1,
-        callback=None,
-        ioloop=None
+         dsn,
+         connection_factory=None,
+         min_pool_size=1,
+         max_pool_size=1,
+         callback=None,
+         ioloop=None,
+         load_balancing_enabled=True
     ):
-        assert size > 0, 'The connection pool size must be a number above 0.'
+        """Asynchronous lazy connection pool.
+
+        The pool manages database connections and passes operations to
+        connections.
+
+        Args:
+            dsn: A string for the data source name to be used to connect to the
+                database. For example: 'dbname=nextdoor user=gisuser'.
+
+        Optional Args:
+            connection_factory: A connection factory to use when creating a new
+                connection in the pool.
+            min_pool_size: An integer specifying the minimum number of
+                connections to keep in the pool.
+            max_pool_size: An integer specifying the maximum number of
+                connections to keep in the pool.
+            ioloop: A tornado ioloop to use for adding asynchronous callbacks
+                and handlers.
+            load_balancing_enabled: Shuffle through connections that are used.
+        """
+
+        assert min_pool_size > 0, 'The connection pool size must be a number above 0.'
+        assert max_pool_size >= min_pool_size, 'The maximum connection max pool_size.'
 
         self.dsn = dsn
-        self.size = size
+        # Initialize size property to match super class.
+        self.size = min_pool_size
         self.closed = False
         self.connection_factory = connection_factory
+        self._load_balancing_enabled = load_balancing_enabled
 
-        self._ioloop = ioloop or IOLoop.instance()
+        self.min_pool_size = min_pool_size
+        self.max_pool_size = max_pool_size
+        self._ioloop = ioloop or tornado_ioloop.IOLoop.instance()
         self._pool = []
+        self._create_min_connection_pool(callback=callback)
 
-        # Create connections
-        def after_pool_creation(n, connection):
-            if n == self.size-1:
-                if callback:
-                    callback()
+    def _create_min_connection_pool(self, callback=None):
+        """Create enough connections to reach the minimum connection pool size.
 
-        for i in range(self.size):
-            self._new(partial(after_pool_creation, i))
+        Note, since this is asynchronous, the pool connections will not
+        necessarily be available when this method returns via callback.
+
+        Args:
+            callback: A callback function to call when all calls to create
+                connections have been dispatched.
+        """
+        connections_to_create = self.min_pool_size - len(self._pool)
+        if connections_to_create > 0:
+
+            def after_pool_creation(n, connection):
+                """Calls the outer callback when all connections are requested.
+
+                Args:
+                    n: An integer for the nth connection created.
+                    connection: The connection created.
+                """
+                if n == connections_to_create - 1:
+                    if callback:
+                        callback()
+
+            for i in range(0, connections_to_create):
+                self._new(partial(after_pool_creation, i))
+        elif callback:
+            callback()
 
     def _new(self, callback=None):
+        """Create a new connection.
+
+        Note: Since creating a new connection is asynchronous, we may fall into
+        a race condition of creating more connections than the maximum allowed
+        in the pool. This could be the case if two callbacks each request a
+        connection and see that there are 1 less than the maximum allowed
+        connections and each request to create a new connection
+        'simultaneously'.
+
+        Args:
+            callback: A callback function to call when the connection has been
+                created.
+
+        Raises:
+            momoko.PoolError if this is called when the pool is already closed.
+        """
+        if self.closed:
+            raise exceptions.PoolError('Tried to create connection when pool is already closed.')
+
         def multi_callback(connection, error):
             if error:
                 raise error
             if callback:
                 callback(connection)
-            self._pool.append(connection)
 
-        Connection(self.dsn, self.connection_factory,
-            multi_callback, self._ioloop)
+            # If we have not reached our maximum pool size add the connection to
+            # the pool, else close the connection
+            if len(self._pool) < self.max_pool_size:
+                self._pool.append(connection)
+            elif not connection.closed:
+                connection.close()
+
+        Connection(self.dsn, self.connection_factory, multi_callback, self._ioloop)
 
     def _get_connection(self):
-        for connection in self._pool:
-            if not connection.busy():
-                return connection
+        """Get a connection from the available pool.
+
+        If there are no available connections, request to create more
+        connections as allowed by pool restrictions.
+        """
+
+        connection = None
+
+        iteration_order = range(len(self._pool))
+        if self._load_balancing_enabled:
+            random.shuffle(iteration_order)
+
+        for index in iteration_order:
+            pool_connection = self._pool[index]
+            if not pool_connection.busy():
+                return pool_connection
+
+        # If we are below the minimum pool size, create more connections.
+        if len(self._pool) < self.min_pool_size:
+            self._create_min_connection_pool()
+        elif len(self._pool) < self.max_pool_size:
+            # If we have not reached our maximum pool size try to create another
+            # connection.
+            self._new()
 
     def transaction(self,
         statements,
@@ -100,6 +198,7 @@ class Pool:
         """
         connection = self._get_connection()
         if not connection:
+            log.warning('Transaction: no connection available, operation queued.')
             return self._ioloop.add_callback(partial(self.transaction,
                 statements, cursor_factory, callback))
 
@@ -119,10 +218,41 @@ class Pool:
         """
         connection = self._get_connection()
         if not connection:
+            log.warning('Execute: no connection available, operation queued.')
             return self._ioloop.add_callback(partial(self.execute,
                 operation, parameters, cursor_factory, callback))
 
-        connection.execute(operation, parameters, cursor_factory, callback)
+        def _callback_wrapper(*args, **kwargs):
+            error = args[1]
+
+            if isinstance(error, psycopg2.DatabaseError):
+                self._pool.remove(connection)
+                self._create_min_connection_pool()
+            callback(*args, **kwargs)
+
+        connection.execute(operation, parameters, cursor_factory, _callback_wrapper)
+
+    def _check_connections(self):
+        """Remove a connection from the connection pool."""
+
+        bad_connections = []
+        for connection in self._pool:
+            try:
+                poll_status = connection.connection.poll()
+            except (psycopg2.DatabaseError, psycopg2.InterfaceError):
+                bad_connections.append(connection)
+            else:
+                if connection._transaction_status() == psycopg2.extensions.TRANSACTION_STATUS_INERROR:
+                    bad_connections.append(connection)
+                elif connection.connection.status == psycopg2.extensions.POLL_ERROR:
+                    bad_connections.append(connection)
+
+        for connection in bad_connections:
+            self._pool.remove(connection)
+            if not connection.closed:
+                connection.close()
+
+        self._create_min_connection_pool()
 
     def callproc(self,
         procname,
@@ -138,6 +268,7 @@ class Pool:
         """
         connection = self._get_connection()
         if not connection:
+            log.warning('Callproc: no connection available, operation queued.')
             return self._ioloop.add_callback(partial(self.callproc,
                 procname, parameters, cursor_factory, callback))
 
@@ -166,17 +297,16 @@ class Pool:
         """
         connection = self._get_connection()
         if not connection:
+            log.warning('Register hstore: no connection available, operation queued.')
             return self._ioloop.add_callback(
                 partial(self.register_hstore, unicode, callback))
 
         connection.register_hstore(True, unicode, callback)
 
     def close(self):
-        """
-        Close the connection pool.
-        """
+        """Removes connections and closes the connection pool."""
         if self.closed:
-            raise PoolError('connection pool is already closed')
+            raise exceptions.PoolError('connection pool is already closed')
 
         for connection in self._pool:
             if not connection.closed:
@@ -227,12 +357,12 @@ class Connection:
             connection_factory=connection_factory or base_connection)
         self.fileno = self.connection.fileno()
         self._transaction_status = self.connection.get_transaction_status
-        self.ioloop = ioloop or IOLoop.instance()
+        self.ioloop = ioloop or tornado_ioloop.IOLoop.instance()
 
         if callback:
             self.callback = partial(callback, self)
 
-        self.ioloop.add_handler(self.fileno, self.io_callback, IOLoop.WRITE)
+        self.ioloop.add_handler(self.fileno, self.io_callback, tornado_ioloop.IOLoop.WRITE)
 
     def io_callback(self, fd=None, events=None):
         try:
@@ -247,9 +377,9 @@ class Connection:
                 if self.callback:
                     self.callback(None)
             elif state == POLL_READ:
-                self.ioloop.update_handler(self.fileno, IOLoop.READ)
+                self.ioloop.update_handler(self.fileno, tornado_ioloop.IOLoop.READ)
             elif state == POLL_WRITE:
-                self.ioloop.update_handler(self.fileno, IOLoop.WRITE)
+                self.ioloop.update_handler(self.fileno, tornado_ioloop.IOLoop.WRITE)
             else:
                 raise psycopg2.OperationalError('poll() returned {0}'.format(state))
 
@@ -283,7 +413,7 @@ class Connection:
         cursor = self.connection.cursor(cursor_factory=cursor_factory or base_cursor)
         cursor.execute(operation, parameters)
         self.callback = partial(callback or _dummy_callback, cursor)
-        self.ioloop.add_handler(self.fileno, self.io_callback, IOLoop.WRITE)
+        self.ioloop.add_handler(self.fileno, self.io_callback, tornado_ioloop.IOLoop.WRITE)
 
     def callproc(self,
         procname,
@@ -324,7 +454,7 @@ class Connection:
         cursor = self.connection.cursor(cursor_factory=cursor_factory or base_cursor)
         cursor.callproc(procname, parameters)
         self.callback = partial(callback or _dummy_callback, cursor)
-        self.ioloop.add_handler(self.fileno, self.io_callback, IOLoop.WRITE)
+        self.ioloop.add_handler(self.fileno, self.io_callback, tornado_ioloop.IOLoop.WRITE)
 
     def mogrify(self, operation, parameters=(), callback=None):
         """
